f6dfbbbb067b5ef28ffb0cd9f22b98e1e17fb52b
Ubuntu <ubuntu@ip-172-31-15-23.us-east-2.compute.internal>
2018-08-14 19:52:48 -0400

    cmake

---
+++ build/CMakeFiles/cbuff.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cbuff.dir/test/temp.cpp.o"
+  "cbuff.pdb"
+  "cbuff"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cbuff.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
